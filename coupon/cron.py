28e2784d721371faece26fc0299ef71da8f9ef33
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2022-01-24 09:52:22 +0530

    Update cron.py

--- coupon/cron.py
+++ coupon/cron.py
@@ -5,7 +5,7 @@ import datetime
 
 import logging
 logger = logging.getLogger(__name__)
-
+# When Coupon Active alert private members for coupon
 def my_cron_job():
     
     logger.info("Coupon Active for Private Members ")
